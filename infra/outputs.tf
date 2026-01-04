f8614d12805632303b1e4eafe165e40ab2b22bf4
Wesley Kaihara <wesleykaihara@gmail.com>
2026-01-04 00:03:13 -0300

    Change tf state object reference

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -17,14 +17,3 @@ output "cognito_user_pool_client_id" {
   description = "ID do Cognito User Pool Client do Orders"
   value       = module.cognito.user_pool_client_id
 }
-
-# Outputs relacionados ao SQS
-output "sqs_kitchen_queue_url" {
-  description = "URL da fila SQS para enviar pedidos para a cozinha"
-  value       = module.sqs_kitchen_orders.sqs_queue_url
-}
-
-output "sqs_payment_queue_url" {
-  description = "URL da fila SQS para receber confirmações de pagamento"
-  value       = data.terraform_remote_state.kitchen_order.outputs.sqs_queue_url
-}
